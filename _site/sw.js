55f8791538aee90ba3efed952552b72bf3eba9a8
Mickhat <root@mickhat.xyz>
2023-09-26 14:34:40 +0200

    test

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230926.142624';
+const cacheName = 'chirpy-20230926.143420';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
