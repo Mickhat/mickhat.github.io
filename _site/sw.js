c60a04da3a1d91d1b94c04f393f7e0e2a8f891a5
Mickhat <root@mickhat.xyz>
2024-02-24 15:13:23 +0100

    minor changes in the website

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240214.115028';
+const cacheName = 'chirpy-20240224.150616';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
